17ad8464bdee4e4990e0f43f4e15dbc2560c1ef3
AkimT13 <akimtarasov@gmail.com>
2024-11-18 15:55:49 -0800

    added icons, added some stuff

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, TextInput } from 'react-native'
+import { View, Text, Button, TextInput, Image } from 'react-native'
 import React from 'react'
 import '../global.css'
 import { useFonts } from "expo-font";
@@ -11,6 +11,9 @@ const App = () => {
   }
     
   return (
+
+
+
     <View style={{ fontFamily: "InriaSerif_Bold",color: "#A1E887"}} className = " text-roomLightGreen flex-1 bg-cover bg-roomDarkBlue">
       <Text className = "text-center text-roomLightGreen font-roomFont mt-72 text-7xl ">RoomSync</Text>
       <View className = 'p-5 w-1/2'>
@@ -20,9 +23,22 @@ const App = () => {
 
 
       </View>
-      <View className = " flex flex-row justify-start snap-center w-96 h-1/3  border self-center ">
-        <TextInput placeholder='Email' className = "border border-b w-96 h-11 bg-roomLightGreen rounded-xl p-6"/>
+      <View className = " flex flex-col justify-start snap-center w-96 h-full  self-center gap-5">
+        <TextInput placeholder='Email' className = "border border-b w-96 h-16 bg-roomLightGreen rounded-xl p-6"/>
+        <TextInput placeholder = 'Password' className = "border border-b w-96 h-16 bg-roomLightGreen rounded-xl p-6"/>
+        <Text className = "ml-60 text-white">Forgot Password?</Text>
       
+        <View className = "flex flex-col text-center bg-roomLightGreen w-96 h-16 rounded-xl pi-6 justify-center mt-4"><Text className = "text-center center font-semibold ">Sign In</Text></View>
+
+        <Text className = 'text-center mt-5 text-white font-semibold'>Or Sign in With</Text>
+        <View className = "flex flex-row gap-4 justify-center w-96 border items-center ">
+          <Image source = {require('../assets/images/signApple.png')} className = 'w-16 h-16 ' />
+          <Image source = {require('../assets/images/googleIcon.png')} className = 'w-16 h-16' />
+        </View>
+
+       
+        
+
         
         
             
---
+++ app/register.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+
+
+export default function Register() {
+
+
+    return(
+        <View className = 'bg-slate-800'>
+            <Text className = "text-blue-500">Hello!</Text>
+        </View>
+    )
+}
