a0a8a8f28a95599924a27ffd862ca9b7ac0004c5
Randy Chen <Randychen168668@gmail.com>
2024-11-18 17:41:58 -0800

    Created text fields for register page.

--- app/Auth.jsx
+++ app/Auth.jsx
@@ -28,7 +28,7 @@ export default function Auth() {
           Sign In
         </Text>
       </View>
-      <View className=" flex flex-col justify-start snap-center w-96 h-full  self-center gap-5">
+      <View className="flex flex-col justify-start snap-center w-96 h-full self-center gap-5">
         <TextInput
           placeholder="Email"
           className="border border-b w-96 h-16 bg-roomLightGreen rounded-xl p-6"
@@ -73,10 +73,38 @@ export default function Auth() {
       </View>
     </View>
   ) : (
-    <View className="flex bg-roomDarkBlue w-full h-full justify-center">
+    <View className="flex-1 bg-cover bg-roomDarkBlue">
       <Pressable onPress={toggle}>
-        <Text className="text-white">Press to go back</Text>
+        <View className="absolute top-20 left-5 flex flex-row items-center justify-center bg-roomPink px-8 py-3 rounded-full shadow-md">
+          <Text className="text-white text-sm font-medium">Back</Text>
+        </View>
       </Pressable>
+      <View>
+        <Text className="font-bold color-white top-36 left-10 text-4xl">Create your account</Text>
+      </View>
+      <View className="flex flex-col justify-start snap-center w-96 h-full self-center gap-5">
+        <TextInput
+        placeholder="Full Name"
+        className="border border-b w-96 h-16 bg-roomLightGreen rounded-xl p-6 top-44"
+        />
+        <TextInput
+        placeholder="Enter Email"
+        className="border border-b w-96 h-16 bg-roomLightGreen rounded-xl p-6 top-44"
+        />
+        <TextInput
+        placeholder="Enter Password"
+        className="border border-b w-96 h-16 bg-roomLightGreen rounded-xl p-6 top-44"
+        />
+        <TextInput
+        placeholder="Confirm Password"
+        className="border border-b w-96 h-16 bg-roomLightGreen rounded-xl p-6 top-44"
+        />
+        <Pressable>
+        <View className="flex flex-col text-center bg-roomLightGreen w-96 h-16 rounded-xl pi-6 justify-center top-52">
+            <Text className="text-center center font-semibold">Sign In</Text>
+          </View>
+        </Pressable>
+      </View>
     </View>
   );
 }
